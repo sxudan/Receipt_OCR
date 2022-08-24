99c430fb6473b46f09b9aae0535e18411078eef8
sudayn <sudosuwal@gmail.com>
2022-08-24 13:23:39 +1000

    version1

--- receiptocr_web/src/components/Transaction/Transaction.js
+++ receiptocr_web/src/components/Transaction/Transaction.js
@@ -6,26 +6,31 @@ import Container from 'react-bootstrap/Container';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Tags from '../Tags/Tags';
+import {dbRef} from '../../firebase'
+import {get, child} from 'firebase/database'
 
 
 function Transaction(props) {
     const [transactions, setTransactions] = useState([])
 
     function getTransactions() {
-        axios.get(`https://new-project-1557054001172.firebaseio.com/Receipts/${props.profile.googleId}.json`)
-          .then(res => {
-            var t = []
+
+      get(child(dbRef, `Receipts/${props.profile.googleId}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          var t = []
             var amt = 0
-            if(res.data) {
-              for(var key in res.data.transactions) {
-                if(res.data.transactions[key].type == "income") {
-                  amt += parseFloat(res.data.transactions[key].amount)
+            const data = snapshot.val()
+            if(data) {
+              for(var key in data.transactions) {
+                if(data.transactions[key].type == "income") {
+                  amt += parseFloat(data.transactions[key].amount)
                   console.log(amt)
                 } else {
-                  amt -= parseFloat(res.data.transactions[key].amount)
+                  amt -= parseFloat(data.transactions[key].amount)
                   console.log(amt)
                 }
-                t.push(res.data.transactions[key])
+                t.push(data.transactions[key])
               }
             }
             props.onBalance({...props.balance, amount: Math.round(amt * 100) / 100})
@@ -34,7 +39,13 @@ function Transaction(props) {
               return d1 < d2
             })
             setTransactions(t)
-          })
+        } else {
+          console.log("No data available");
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+
       }
     
       useEffect(() => () => {
@@ -60,7 +71,7 @@ function Transaction(props) {
                     <Row><h6>{x.category}</h6></Row>
                     <Row><p>{x.date}</p></Row>
                   </Col>
-                  <Col sm={3} style={{color: x.type === "income" ? "green" : "red"}}>{x.amount}</Col>
+                  <Col sm={3} style={{color: x.type === "income" ? "green" : "red"}}>{x.currency + x.amount}</Col>
                   <Col sm={1}>
                     <Tags color="white" bgColor="red" text={x.image.includes("pdf")? "pdf": "image"}/>
                   </Col>
